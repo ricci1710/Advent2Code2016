8d5e4abd10d6011807a147b52c02bf16476eecf3
Rico Kunz <ricci1710@gmail.com>
2023-01-30 16:22:50 +0100

    start

---
+++ lib/base/day.dart
@@ -0,0 +1,18 @@
+class Day {
+  final String dayName;
+
+  Day(this.dayName);
+/*
+  Day.demo(this.dayName) {
+    loadFileFromAsset();
+  }
+
+  Day.life(this.dayName);
+
+  void loadFileFromAsset() async {
+    String response = await rootBundle.loadString('text_notes/my_text.txt');
+    print(response);
+  }
+
+   */
+}
